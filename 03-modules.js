5b88d3e1c0fddafde3da9a97394df396e74b691a
DEBJIT KUNDU <debjitkundu1117@gmail.com>
2023-12-31 13:36:50 +0530

    changed the file names

--- 03-modules.js
+++ 03-modules.js
@@ -3,16 +3,16 @@
 
 //commonjs, every file is a module (by default)
 
-const names = require('./4-names');
+const names = require('./04-names');
 console.log(names)
 
 /* sayHi('deeya')
 sayHi(jhon)
 sayHi(peter) */
 
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavour')
+const sayHi = require('./05-utils')
+const data = require('./06-alternative-flavour')
     /* console.log(data) */
-require('./7-mind-grenade')
+require('./07-mind-grenade')
 sayHi('debjit')
 sayHi(names.jhon)
